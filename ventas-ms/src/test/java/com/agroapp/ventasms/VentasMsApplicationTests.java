e752d735e0862056305c8a3a65bd9f2f3aab3f6e
Jhon Romero <jhon.jromerot@gmail.com>
2021-11-28 18:58:11 -0500

    Microservicio de ventas

---
+++ ventas-ms/src/test/java/com/agroapp/ventasms/VentasMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agroapp.ventasms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentasMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
